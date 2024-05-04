65a917c7a4bfc67dbff18887d20c86f13fd4af2a
Orlando Barboza <124634329+B4rz99@users.noreply.github.com>
2024-05-04 13:48:44 -0500

    Patient names on Selector once confirmed.

--- webfront/src/assets/selector.js
+++ webfront/src/assets/selector.js
@@ -12,88 +12,95 @@ import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 
 export default function BasicSelect({ onSelectorChange }) {
-  const [devices, setDevices] = useState([]); // State to store devices data
-  const [selectedDeviceIds, setSelectedDeviceIds] = useState([]); // State to store selected device IDs
-  const [openSnackbar, setOpenSnackbar] = useState(false); // State for showing snackbar
+    const [devices, setDevices] = useState([]); // State to store devices data
+    const [selectedDeviceIds, setSelectedDeviceIds] = useState([]); // State to store selected device IDs
+    const [openSnackbar, setOpenSnackbar] = useState(false); // State for showing snackbar
 
-  useEffect(() => {
-    // Function to fetch devices data
-    async function fetchDevices() {
-      try {
-        const response = await axios.get(`${API_URL}/api/allDevices`);
-        if (response.status === 200) {
-          // Store devices data in state
-          const devicesData = response.data;
-          setDevices(devicesData);
+    useEffect(() => {
+        // Function to fetch devices data
+        async function fetchDevices() {
+            try {
+                const response = await axios.get(`${API_URL}/api/allDevices`);
+                if (response.status === 200) {
+                    // Store devices data in state
+                    const devicesData = response.data;
+                    setDevices(devicesData);
 
-          // Set all device IDs as initially selected
-          const allDeviceIds = devicesData.map(device => device.DeviceId);
-          setSelectedDeviceIds(allDeviceIds);
+                    // Set all device IDs as initially selected
+                    const allDeviceIds = devicesData.map(device => device.DeviceId);
+                    setSelectedDeviceIds(allDeviceIds);
 
-          // Call onSelectorChange function with all device IDs
-          onSelectorChange(allDeviceIds);
-        } else {
-          console.error('Error fetching devices data:', response.statusText);
+                    // Call onSelectorChange function with all device IDs
+                    onSelectorChange(allDeviceIds);
+                } else {
+                    console.error('Error fetching devices data:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error during the request:', error.message);
+            }
         }
-      } catch (error) {
-        console.error('Error during the request:', error.message);
-      }
-    }
 
-    // Fetch devices data when the component mounts
-    fetchDevices();
-  }, []);
+        // Fetch devices data when the component mounts
+        fetchDevices();
+    }, []);
 
-  const handleChange = (event) => {
-    const selectedValues = event.target.value;
+    const handleChange = (event) => {
+        const selectedValues = event.target.value;
 
-    // If no options are selected, display a warning and return early
-    if (selectedValues.length === 0) {
-      setOpenSnackbar(true);
-      return;
-    }
+        // If no options are selected, display a warning and return early
+        if (selectedValues.length === 0) {
+            setOpenSnackbar(true);
+            return;
+        }
 
-    // Update selected device IDs state
-    setSelectedDeviceIds(selectedValues);
-    
-    // Call onSelectorChange function with selected device IDs
-    onSelectorChange(selectedValues);
-  };
+        // Update selected device IDs state
+        setSelectedDeviceIds(selectedValues);
+        
+        // Call onSelectorChange function with selected device IDs
+        onSelectorChange(selectedValues);
+    };
 
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
-  };
+    const handleCloseSnackbar = () => {
+        setOpenSnackbar(false);
+    };
 
-  return (
-    <Box sx={{ minWidth: 200 }}>
-      <FormControl sx={{ m: 1, width: 200 }}>
-        <InputLabel id="multi-select-label">Pacientes</InputLabel>
-        <Select
-          labelId="multi-select-label"
-          id="multi-select"
-          multiple
-          value={selectedDeviceIds}
-          onChange={handleChange}
-          renderValue={(selected) => selected.join(', ')}
-        >
-          {devices.map((device) => (
-            <MenuItem key={device.DeviceId} value={device.DeviceId}>
-              <Checkbox checked={selectedDeviceIds.includes(device.DeviceId)} />
-              <ListItemText primary={device.Name} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {/* Snackbar for warning */}
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={handleCloseSnackbar}
-      >
-        <Alert onClose={handleCloseSnackbar} severity="warning" sx={{ width: '100%' }}>
-          Mínimo debe haber un paciente debe ser seleccionado.
-        </Alert>
-      </Snackbar>
-    </Box>
-  );
+    // Render the names of selected devices instead of their IDs
+    const renderSelectedDeviceNames = (selected) => {
+        const selectedDevices = devices.filter(device => selected.includes(device.DeviceId));
+        const selectedDeviceNames = selectedDevices.map(device => device.Name);
+        return selectedDeviceNames.join(', ');
+    };
+
+    return (
+        <Box sx={{ minWidth: 200 }}>
+            <FormControl sx={{ m: 1, width: 200 }}>
+                <InputLabel id="multi-select-label">Pacientes</InputLabel>
+                <Select
+                    labelId="multi-select-label"
+                    id="multi-select"
+                    multiple
+                    value={selectedDeviceIds}
+                    onChange={handleChange}
+                    renderValue={renderSelectedDeviceNames} // Use the new function to render device names
+                >
+                    {devices.map((device) => (
+                        <MenuItem key={device.DeviceId} value={device.DeviceId}>
+                            <Checkbox checked={selectedDeviceIds.includes(device.DeviceId)} />
+                            <ListItemText primary={device.Name} />
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            {/* Snackbar for warning */}
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={3000}
+                onClose={handleCloseSnackbar}
+            >
+                <Alert onClose={handleCloseSnackbar} severity="warning" sx={{ width: '100%' }}>
+                    Mínimo debe haber un paciente debe ser seleccionado.
+                </Alert>
+            </Snackbar>
+        </Box>
+    );
 }
